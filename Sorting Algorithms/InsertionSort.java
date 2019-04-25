c17830a668a9124c005447c30297994057f6f92c
Long Phan <lnp26@case.edu>
2019-04-24 23:49:39 -0400

    Update InsertionSort.java

--- Sorting Algorithms/InsertionSort.java
+++ Sorting Algorithms/InsertionSort.java
@@ -4,31 +4,20 @@ public class InsertionSort {
 		
 		// unsorted Index will start at 1 and traverse right
 		for(int unsortedIndex = 1; unsortedIndex < array.length; unsortedIndex++) {
-			array = insertionSort(array, unsortedIndex);
+			int swapElement = array[unsortedIndex];
+			
+			int i;
+			for(i = unsortedIndex; i > 0 && array[i -1 ] > swapElement; i--) {
+					array[i] = array[i - 1];
+			}
+			
+			array[i] = swapElement;
 			printArray(array, unsortedIndex);
-		}
-		return array;
-	}
-	
-	private int[] insertionSort(int[] array, int unsortedIndex) {
-		
-		int swapIndex = unsortedIndex;
-		// Traverse left to find the value smaller than array[unsortedIndex]
-		for(int i = unsortedIndex; i >= 0; i--) {
-			if(array[i] > array[swapIndex])
-				swapIndex = i;
-		}
-		
-		return swap(array, unsortedIndex, swapIndex);
-	}
-	
-	
-	private int[] swap(int[] array, int index1, int index2) {
-		int temp = array[index2];
-		array[index2] = array[index1];
-		array[index1] = temp;
-		return array;
 
+		}
+		for(Integer i: array)
+			System.out.print(i + " ");
+		return array;
 	}
 	
 	private void printArray(int[] array, int unsortedIndex) {
@@ -43,7 +32,7 @@ public class InsertionSort {
 	
 	public static void main(String[] args) {
 		InsertionSort sort = new InsertionSort();
-		sort.insertionSort(new int[] { 80, 10, 50, 70 });		
+		sort.insertionSort(new int[] {20, 80, 10, 50, 70 });		
 	}
 
 }
